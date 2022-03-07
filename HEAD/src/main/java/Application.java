ded953703bd2daea01ca68c12c5ac4be3a6c79bd
Vsevolod <lingelcry@gmail.com>
2022-03-07 23:54:12 +0300

    Sum commit

--- HEAD/src/main/java/Application.java
+++ HEAD/src/main/java/Application.java
@@ -2,4 +2,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    private int sum(int a , int b) {
+        return a + b ;
+    }
+
+
 }
